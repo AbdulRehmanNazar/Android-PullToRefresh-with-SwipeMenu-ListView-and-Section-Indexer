4e79a8ca185b851bfda649bef6a05f91c62a4e6f
Abdul Rehman Nazar <a.rehman.nazar@gmail.com>
2015-12-09 10:34:48 +0500

    Section Indexer implementation

--- AndroidPullToRefreshwithSwipeMenuListViewandSection-Indexer/app/src/main/java/com/enode/solutions/swipemu/pulltorefresh/sectionindexer/MainActivity.java
+++ AndroidPullToRefreshwithSwipeMenuListViewandSection-Indexer/app/src/main/java/com/enode/solutions/swipemu/pulltorefresh/sectionindexer/MainActivity.java
@@ -1,52 +1,177 @@
 package com.enode.solutions.swipemu.pulltorefresh.sectionindexer;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import com.enode.solutions.swipemu.pulltorefresh.sectionindexer.CustomListView.PinnedHeaderPullToRefreshSwipeMenuListView;
+import com.enode.solutions.swipemu.pulltorefresh.sectionindexer.SectionIndexer.SideBladeView;
+import com.enode.solutions.swipemu.pulltorefresh.sectionindexer.adapter.PinnedHeaderSectionAdapter;
+import com.enode.solutions.swipemu.pulltorefresh.sectionindexer.models.Countries;
+import com.enode.solutions.swipemu.pulltorefresh.sectionindexer.parsers.CountriesParser;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+{
+	private static final String ENG_FORMAT = "[A-Za-z0-9_\\-]+";
+	private PinnedHeaderPullToRefreshSwipeMenuListView mListView;
+	private SideBladeView mLetter;
+	private PinnedHeaderSectionAdapter mAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+	private List<String> mSections;
+	private Map<String, List<Countries>> mMap;
+	private List<Integer> mPositions;
+	private Map<String, Integer> mIndexer;
+	private List<String> list11;
+	private ArrayList<Countries> countriesList;
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.content_main);
+		initData();
+		initView();
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+	public String loadJSONFromAsset()
+	{
+		String json = null;
+		try
+		{
+			InputStream is = getAssets().open("country.json");
+			int size = is.available();
+			byte[] buffer = new byte[size];
+			is.read(buffer);
+			is.close();
+			json = new String(buffer, "UTF-8");
+		}
+		catch (IOException ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+		return json;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+	private void initData()
+	{
+		String lang = Locale.getDefault().getCountry().toLowerCase();
+		// lang ="cn";
+		countriesList = new ArrayList<Countries>();
+		String json = loadJSONFromAsset();
+		countriesList = new CountriesParser().parseData(json);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+		// extract initials
+		String initial;
 
-        return super.onOptionsItemSelected(item);
-    }
+		for (int i = 0; i < countriesList.size(); i++)
+		{
+			initial = countriesList.get(i).getCountryEngName().substring(0, 1);
+			countriesList.get(i).setInitial(initial);
+		}
+
+		Collections.sort(countriesList, new Comparator<Countries>()
+		{
+			@Override
+			public int compare(Countries lhs, Countries rhs)
+			{
+				return lhs.getCountryEngName().compareTo(rhs.getCountryEngName());
+			}
+		});
+
+		list11 = new ArrayList<String>();
+		for (int i = 0; i < countriesList.size(); i++)
+		{
+			String nameToShow;
+
+			nameToShow = countriesList.get(i).getCountryEngName() + " ( +" + countriesList.get(i).getCountryPrefixCode() + ")";
+
+			list11.add(nameToShow);
+
+		}
+
+		mSections = new ArrayList<String>(); // section list
+		mMap = new HashMap<String, List<Countries>>(); // section name ->
+		mPositions = new ArrayList<Integer>();
+		mIndexer = new HashMap<String, Integer>();
+
+		for (int i = 0; i < countriesList.size(); i++)
+		{
+
+			String firstName = countriesList.get(i).getInitial();
+			if (firstName.matches(ENG_FORMAT))
+			{
+				if (mSections.contains(firstName))
+				{
+					mMap.get(firstName).add(countriesList.get(i));
+				}
+				else
+				{
+					mSections.add(firstName);
+					List<Countries> list = new ArrayList<Countries>();
+					list.add(countriesList.get(i));
+					mMap.put(firstName, list);
+				}
+			}
+			else
+			{
+
+				if (mSections.contains("#"))
+				{
+					mMap.get("#").add(countriesList.get(i));
+				}
+				else
+				{
+					mSections.add("#");
+					List<Countries> list = new ArrayList<Countries>();
+					list.add(countriesList.get(i));
+					mMap.put("#", list);
+				}
+			}
+		}
+
+		Collections.sort(mSections);
+
+		int position = 0;
+		for (int i = 0; i < mSections.size(); i++)
+		{
+			mIndexer.put(mSections.get(i), position);
+			mPositions.add(position);
+			position += mMap.get(mSections.get(i)).size();
+		}
+	}
+
+	private void initView()
+	{
+		// TODO Auto-generated method stub
+		mListView = (PinnedHeaderPullToRefreshSwipeMenuListView) findViewById(R.id.friends_display);
+		mLetter = (SideBladeView) findViewById(R.id.friends_myletterlistview);
+		mLetter.setOnItemClickListener(new SideBladeView.OnItemClickListener()
+		{
+
+			@Override
+			public void onItemClick(String s)
+			{
+				if (mIndexer.get(s) != null)
+				{
+					mListView.setSelection(mIndexer.get(s));
+				}
+			}
+		});
+		mAdapter = new PinnedHeaderSectionAdapter(this, list11, mSections, mPositions);
+		mListView.setAdapter(mAdapter);
+		mListView.setOnScrollListener(mAdapter);
+		mListView.setPinnedHeaderView(LayoutInflater.from(this).inflate(R.layout.listview_item, mListView, false));
+	}
+
 }
